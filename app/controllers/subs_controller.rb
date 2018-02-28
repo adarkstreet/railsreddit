8a34b6c439310cfe6ebe2c1af19228fa1cafcea3
Andrew Street <andrew_street@live.com>
2018-02-28 11:41:16 -0700

    controllers

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,13 +1,51 @@
 class SubsController < ApplicationController
+  before_action :set_sub, only: [:show, :edit, :update, :destroy]
   def index
+    @subs = Subs.all
   end
 
   def show
   end
 
   def new
+    @sub = Sub.new
   end
 
   def edit
   end
+
+  def create
+    @sub = Sub.new(sub_params)
+
+    if @sub.save
+      redirect_to sub_path(@sub)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @sub.uodate(sub_params)
+      redirect_to sub_path(@sub)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sub.destroy
+    redirect_to subs_path
+  end
+
+  private
+    def sub_params
+      params.require(:sub).permit(:name)
+    end
+
+    def set_sub
+      @sub = Sub.find(params(:id))
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'subs#index'
 
-  resources :subs
+  resources :subs do
+    resources :topics
+  end
+
+  
 end
